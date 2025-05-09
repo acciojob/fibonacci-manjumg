21cd7da19db39176e460c889dd637807e3837d03
Manjunath M Goni <manjugoni007@gmail.com>
2025-05-09 12:36:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function fibonacci(num) {
-    if (num === 0) return 0;
-    if (num === 1) return 0;
-    if (num === 2) return 1;
+  if (num === 0) return 0;
+  if (num === 1) return 0;
+  if (num === 2) return 1;
 
-    let a = 0, b = 1;
-    for (let i = 3; i <= num; i++) {
-        [a, b] = [b, a + b];
-    }
-    return b;
-	console.log(fibonacci(num));
+  let a = 0, b = 1, fib;
+  for (let i = 3; i <= num; i++) {
+    fib = a + b;
+    a = b;
+    b = fib;
+  }
+  return fib;
 }
- // Output: 0
 
+module.exports = fibonacci;
